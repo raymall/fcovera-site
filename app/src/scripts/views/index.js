ef8b2eef5143949136b02c6e8e90a1b2ed40881c
Raymall Perez <raymall.perez@gmail.com>
2019-01-17 12:26:44 -0400

    Fixed revisions from Jan 16th 2019

--- app/src/scripts/views/index.js
+++ app/src/scripts/views/index.js
@@ -1,15 +1,17 @@
 import Swup from 'swup';
 // import printMe from './me.js';
-
-const swup = new Swup();
-
-window.addEventListener('load', function() {
-  document.querySelector('body').classList.remove('preload');
-}, false);
+  
+const swup = new Swup({
+  debugMode: true
+});
 
 swup.on('pageView', function () {
   document.querySelector('body').classList.remove('preload');
 });
+
+window.addEventListener('load', function() {
+  document.querySelector('body').classList.remove('preload');
+}, false);
 
 // (function() {
 //   printMe();
