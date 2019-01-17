e23761dc0651bd2ff3caac38bafaefd51481f4e6
Raymall Perez <raymall.perez@gmail.com>
2019-01-17 15:28:54 -0400

    Added Work and Writings page carousel

--- app/src/scripts/views/index.js
+++ app/src/scripts/views/index.js
@@ -1,17 +1,36 @@
 import Swup from 'swup';
+import Glide from '@glidejs/glide';
 // import printMe from './me.js';
   
 const swup = new Swup({
   debugMode: true
 });
 
-swup.on('pageView', function () {
-  document.querySelector('body').classList.remove('preload');
-});
+const bodyElem = document.querySelector('body');
 
-window.addEventListener('load', function() {
-  document.querySelector('body').classList.remove('preload');
-}, false);
+const loadFn = function() {
+  bodyElem.classList.remove('preload');
+
+  if (bodyElem.classList.contains('--work') || bodyElem.classList.contains('--writings'))
+  new Glide('.glide', {
+    type: 'carousel',
+    startAt: 0,
+    focusAt: 0,
+    gap: 40,
+    autoplay: false,
+    hoverpause: true,
+    peek: { before: 0, after: 140 },
+    breakpoints: {
+      640: {
+        gap: 30,
+        peek: { before: 0, after: 60 },
+      }
+    }
+  }).mount();
+}
+
+swup.on('pageView', loadFn);
+window.addEventListener('load', loadFn, false);
 
 // (function() {
 //   printMe();
--- app/src/scripts/views/me.js
+++ app/src/scripts/views/me.js
@@ -1,4 +1,7 @@
 import meImg from '../../assets/images/me.jpg';
+// import work1 from '../../assets/images/work-1.jpg';
+// import work2 from '../../assets/images/work-2.jpg';
+// import work3 from '../../assets/images/work-3.jpg';
 
 export default function printMe() {
   console.log('I get called from print.js!!!');
